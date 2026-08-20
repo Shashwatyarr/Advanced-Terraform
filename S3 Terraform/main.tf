12cebc22ca010c06123d697caa21fdcb8639372a
Shashwat Srivastava <shashwat.2428it2297@kiet.edu>
2026-08-20 23:06:06 +0530

    s3 from terraform

---
+++ S3 Terraform/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+#create s3 bucket
+resource "aws_s3_bucket" "first_bucket" {
+  bucket = "s3bucketwithterraform03972554"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
